429209d614f1a60318ca9d5f265e8c075a3b1a85
Francisco Fernandes <francisco.fernandes.j@gmail.com>
2016-07-07 15:11:07 -0300

    Created function to add dict based rectangles to Map.rectangles list

--- flask_googlemaps/__init__.py
+++ flask_googlemaps/__init__.py
@@ -16,6 +16,8 @@ class Map(object):
                  varname='map',
                  style="height:300px;width:300px;margin:0;",
                  cls="map",
+                 rectangles=None,
+                 drawing=False,
                  zoom_control=True,
                  maptype_control=True,
                  scale_control=True,
@@ -32,8 +34,11 @@ class Map(object):
         self.maptype = maptype
         self.markers = []
         self.build_markers(markers)
-        self.identifier = identifier
+        # Following the same pattern of building markers for rectangles objs
+        self.rectangles = []
+        self.build_rectangles(rectangles)
 
+        self.identifier = identifier
         self.zoom_control = zoom_control
         self.maptype_control = maptype_control
         self.scale_control = scale_control
@@ -80,6 +85,55 @@ class Map(object):
         if 'lat' not in kwargs or 'lng' not in kwargs:
             raise AttributeError('lat and lng required')
         self.markers.append(kwargs)
+
+    def add_rectangle(self, north_east=None, south_west=None, **kwargs):
+        """ Adds a rectangle dict to the Map.rectangles attribute
+
+        The Google Maps API describes a rectangle using the LatLngBounds
+        object, which defines the bounds to be drawn. The bounds use the
+        concept of 2 delimiting points, a northeast and a southwest points,
+        described as the parameters north_east and south_west of this function.
+
+        Args:
+            north_east (tuple): The north and east bounds of the rectangle
+            south_west (tuple): The south and west bounds of the rectangle
+
+        .. _LatLngBounds class:
+                https://developers.google.com/maps/documentation/javascript/reference#LatLngBounds
+
+        """
+
+        if north_east:
+            kwargs['bounds'] = {}
+            kwargs['bounds']['north'] = north_east[0]
+            kwargs['bounds']['east'] = north_east[1]
+
+        if south_west:
+            kwargs['bounds']['south'] = south_west[0]
+            kwargs['bounds']['west'] = south_west[1]
+
+        if 'bounds' not in kwargs:
+            raise AttributeError('bounds required to build rectangles')
+
+        if 'bounds' in kwargs \
+                and {'north', 'east', 'south', 'west'} \
+                != kwargs['bounds'].keys():
+            raise AttributeError('bounds directions required to rectangles')
+
+        if 'stroke_color' not in kwargs:
+            kwargs['stroke_color'] = '#FF0000'
+
+        if 'stroke_opacity' not in kwargs:
+            kwargs['stroke_opacity'] = .8
+
+        if 'stroke_weight' not in kwargs:
+            kwargs['stroke_weight'] = 2
+
+        if 'fill_color' not in kwargs:
+            kwargs['fill_color'] = '#FF0000'
+
+        if 'fill_opacity' not in kwargs:
+            kwargs['fill_opacity'] = .3
 
     def render(self, *args, **kwargs):
         return render_template(*args, **kwargs)
