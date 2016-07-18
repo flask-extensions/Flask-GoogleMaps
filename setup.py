19476ed16f1b3647bff39f72075762d345da0705
Bruno Rocha <rochacbruno@gmail.com>
2016-07-18 20:36:48 -0300

    Fix #27 makes map variable global using map.varname

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-GoogleMaps',
-    version='0.2.0',
+    version='0.2.1',
     license='MIT',
     description='Small extension for Flask to make using Google Maps easy',
     long_description=open('README.md').read(),
