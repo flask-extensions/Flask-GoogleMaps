b626f28ff4408d41a7cee0855fb84bc319cc8615
Bruno Rocha <rochacbruno@gmail.com>
2016-10-07 12:46:11 -0300

    Bump 0.2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-GoogleMaps',
-    version='0.2.2',
+    version='0.2.3',
     license='MIT',
     description='Small extension for Flask to make using Google Maps easy',
     long_description=open('README.md').read(),
