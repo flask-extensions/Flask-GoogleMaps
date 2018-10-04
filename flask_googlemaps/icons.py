9d6f92fd97d28d12937a9470aa5944a5edf775ca
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-10-04 12:47:11 -0400

    fix typos in paddle options

--- flask_googlemaps/icons.py
+++ flask_googlemaps/icons.py
@@ -7,7 +7,7 @@ and
 http://mabp.kiev.ua/2010/01/12/google-map-markers/
 """
 
-__all__ = ['dots', 'alpha','shapes','pushpin','paddle']
+__all__ = ['dots', 'alpha', 'shapes', 'pushpin', 'paddle']
 
 
 class Icon(object):
@@ -67,17 +67,17 @@ paddle = Icon(
     base_url='//maps.google.com/mapfiles/kml/paddle/{0}.png',
     options=['1-lv','2-lv','3-lv','4-lv','5-lv','6-lv','7-lv','8-lv','9-lv','10-lv',
              '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
+             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
              'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
              'X', 'W', 'Y', 'Z',
-             'blu-blank-lv', 'blue-blank', 'blu-circle-lv', 'blu-circle', 'blu-diamond-lv', 'blu-diamond', 'blu-square-lv', 'blu-square', 'blu-stars-lv', 'blu-stars', 
+             'blu-blank-lv', 'blu-blank', 'blu-circle-lv', 'blu-circle', 'blu-diamond-lv', 'blu-diamond', 'blu-square-lv', 'blu-square', 'blu-stars-lv', 'blu-stars', 
              'grn-blank-lv', 'grn-blank', 'grn-circle-lv', 'grn-circle', 'grn-diamond-lv', 'grn-diamond', 'grn-square-lv', 'grn-square', 'grn-stars-lv', 'grn-stars', 
              'ltblu-blank', 'ltblu-circle', 'ltblu-diamond', 'ltblu-square', 'ltblu-stars',
              'pink-blank', 'pink-circle', 'pink-diamond', 'pink-square', 'pink-stars',
              'purple-blank', 'purple-circle-lv', 'purple-circle', 'purple-diamond-lv', 'purple-diamond', 'purple-square-lv', 'purple-square', 'purple-stars-lv', 'purple-stars',
              'red-circle-lv', 'red-circle', 'red-diamond-lv', 'red-diamond', 'red-square-lv', 'red-square', 'red-stars-lv', 'red-stars',
-             'white-blank', 'wht-blank-lv', 'wht-circle-lv', 'wht-circle', 'wht-diamond-lv', 'wht-diamond', 'wht-square-lv', 'wht-square', 'wht-stars-lv', 'wht-stars',
-             'yellow-blank', 'ylw-blank-lv', 'ylw-circle-lv', 'ylw-circle', 'ylw-diamond-lv', 'ylw-diamond', 'ylw-square-lv', 'ylw-square', 'ylw-stars-lv', 'ylw-stars',
+             'wht-blank', 'wht-blank-lv', 'wht-circle-lv', 'wht-circle', 'wht-diamond-lv', 'wht-diamond', 'wht-square-lv', 'wht-square', 'wht-stars-lv', 'wht-stars',
+             'ylw-blank', 'ylw-blank-lv', 'ylw-circle-lv', 'ylw-circle', 'ylw-diamond-lv', 'ylw-diamond', 'ylw-square-lv', 'ylw-square', 'ylw-stars-lv', 'ylw-stars',
              'orange-blank',  'orange-circle', 'orange-diamond', 'orange-square', 'orange-stars', 
              'go-lv', 'go', 'pause-lv', 'pause', 'stop-lv', 'stop', 'route'
             ]
